51cdad80222a63948cb5f03c256c17031ff012e8
andres Snitcofsky <andres@soviet.com.ar>
2021-04-30 10:55:49 -0300

    textos

--- es/textos.js
+++ es/textos.js
@@ -16,5 +16,16 @@ window.textos = {
   },
 
   "grafico1.title": "Acceso a internet y población",
-  "grafico1.description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  "grafico1.description": "Si comparamos los países por población (diámetro del círculo) y por acceso a internet (color y posición) vemos que una parte considerable de los habitantes de latinoamérica se encuentra en el rango medio/alto de acceso. Sin embargo, algunos países se encuentran relegados con acceso bajo",
+
+  "grafico2.title": "Downloads y acceso a internet",
+  "grafico2.description": "Basándonos en la cantidad de descargas de cada app, relativas a la población de cada país, podemos también comparar la llegada que tienen las apps a la población entre países. Aquí se ve una división aún más grande entre apps con alta incidencia y apps con mucha menor penetración.",
+
+  "grafico3.title": "¿Cuánta seguridad ofrecen las apps para los datos que recogen?",
+  "grafico3.description": "Si agrupamos las apps por los diferentes ejes que consideramos en cuanto a seguridad, se generan también, tres grupos principales:",
+
+  "grafico4.title": "¿Qué datos de usario recolectan?",
+  "grafico4.description": "Agrupando los datos que recolectan en tres grandes grupos, podemos analizar como se posicionan las diferentes apps respecto a la recolección de los mismos:"
+
+  //"grafico1.description": "Si comparamos los países por población (diámetro del círculo) y por acceso a internet (color y posición) vemos que una parte considerable de los habitantes de latinoamérica se encuentra en el rango <span class='medio'>medio</span>/<span class='alto'>alto</span> de acceso. Sin embargo, algunos países se encuentran relegados con acceso <span class='bajo'>bajo</span>"
 }
