af3df774ade512154789aee76d629031e00ed183
andres Snitcofsky <andres@soviet.com.ar>
2021-06-04 14:07:23 -0300

    corregidos los textos para que sean traducibles

--- es/textos.js
+++ es/textos.js
@@ -19,16 +19,28 @@ window.textos = {
   "grafico1.description": "Si comparamos los países por población (diámetro del círculo) y por acceso a internet (color y posición) vemos que una parte considerable de los habitantes de latinoamérica se encuentra en el rango medio/alto de acceso. Sin embargo, algunos países se encuentran relegados con acceso bajo",
 
   "grafico2.title": "Downloads y acceso a internet",
-  "grafico2.description": "Basándonos en la cantidad de descargas de cada app, relativas a la población de cada país, podemos también comparar la llegada que tienen las apps a la población entre países. Aquí se ve una división aún más grande entre apps con alta incidencia y apps con mucha menor penetración.",
+  "grafico2.description": "Basándonos en la cantidad de descargas de cada iniciativa, relativas a la población de cada país, podemos también comparar la llegada que tienen las iniciativas a la población entre países. Aquí se ve una división aún más grande entre iniciativas con alta incidencia y iniciativas con mucha menor penetración.",
 
-  "grafico3.title": "¿Cuánta seguridad ofrecen las apps para los datos que recogen?",
-  "grafico3.description": "Si agrupamos las apps por los diferentes ejes que consideramos en cuanto a seguridad, se generan también, tres grupos principales:",
+  "grafico3.title": "¿Cuánta seguridad ofrecen las iniciativas para los datos que recogen?",
+  "grafico3.description": "Si agrupamos las iniciativas por los diferentes ejes que consideramos en cuanto a seguridad, se generan también, tres grupos principales:",
+  "grafico3.step0": 'Si agrupamos las iniciativas por sus características de seguridad podemos ver tres grandes grupos: ',
+  "grafico3.step1": 'Las que <b>no informan</b> sobre las medidas de seguridad, el <b>codigo es privativo</b>, la información se guarda en <b>servidores privados</b> y <b>fuera del país</b>, como: <span id="apps1"></span>',
+  "grafico3.step2": 'Las que <b>sí informan</b> sobre las medidas de seguridad, el <b>codigo es abierto</b>, la información se guarda en <b>servidores públicos</b> y <b>en el país</b>, como: <span id="apps2"></span>',
+  "grafico3.step3": 'Las que se encuentran en el medio, con una combinación de las variantes, como: <span id="apps3"></span>',
 
   "grafico4.title": "¿Qué datos de usario recolectan?",
-  "grafico4.description": "Agrupando los datos que recolectan en tres grandes grupos, podemos analizar como se posicionan las diferentes apps respecto a la recolección de los mismos:",
+  "grafico4.description": "Agrupando los datos que recolectan en tres grandes grupos, podemos analizar como se posicionan las diferentes iniciativas respecto a la recolección de los mismos:",
+  "grafico4.step0": "En cuanto a <b>datos personales</b> (edad, documento, género), las aplicaciones se ubican en el siguiente espectro en cuanto a la cantidad de datos que recolectan",
+  "grafico4.step1": "En cuanto a <b>datos de ubicación</b> (dirección, ubicación), las aplicaciones se ubican en el siguiente espectro en cuanto a la cantidad de datos que recolectan",
+  "grafico4.step2": "En cuanto a <b>datos sensibles</b> (Fotografía, datos de salud), las aplicaciones se ubican en el siguiente espectro en cuanto a la cantidad de datos que recolectan",
 
-  "grafico5.title": "La importancia del consentimiento en los datos recogidos por las apps",
-  "grafico5.description": "Qué sería mejor o no del punto de vista de la protección de los datos"
+
+  "grafico5.title": "La importancia del consentimiento en los datos recogidos por las iniciativas",
+  "grafico5.description": "Qué sería mejor o no del punto de vista de la protección de los datos",
+  "grafico5.description": 'Is the data collection / processing preceded by a clear affirmative act establishing a freely given, specific, informed and unambiguous indication of agreement by the data subject regarding the collection and / or processing of personal data related to it or the?',
+  "grafico5.description": 'How is consent obtained?',
+  "grafico5.description": 'Does the initiative allow users to have effective control over uses?',
+  "grafico5.description": 'Can the interested party withdraw their consent to the initiative at any time?'
 
   //"grafico1.description": "Si comparamos los países por población (diámetro del círculo) y por acceso a internet (color y posición) vemos que una parte considerable de los habitantes de latinoamérica se encuentra en el rango <span class='medio'>medio</span>/<span class='alto'>alto</span> de acceso. Sin embargo, algunos países se encuentran relegados con acceso <span class='bajo'>bajo</span>"
 }
