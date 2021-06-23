46ea65c5af0f01309ceaa3520296003c8d2d8fd0
Lu Pa <lupa@inventati.org>
2021-06-23 17:16:07 -0300

    agregado portu y corregidos varios typos

---
+++ pt/textos.js
@@ -0,0 +1,46 @@
+window.textosLib = window.textosLib || {};
+window.textosLib.pt = {
+  lang: 'pt',
+  d3: {
+    "decimal": ",",
+    "thousands": ".",
+    "grouping": [3],
+    "currency": ["$", ""],
+    "dateTime": "%a %b %e %X %Y",
+    "date": "%d/%m/%Y",
+    "time": "%H:%M:%S",
+    "periods": ["AM", "PM"],
+    "days": ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"],
+    "shortDays": ["Dom", "Lun", "Mar", "Mi", "Jue", "Vie", "Sab"],
+    "months": ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
+    "shortMonths": ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"]
+  },
+
+  "grafico1.title": "Acceso a internet y población",
+  "grafico1.description": "Si comparamos los países por población (diámetro del círculo) y por acceso a internet (color y posición) vemos que una parte considerable de los habitantes de latinoamérica se encuentra en el rango medio/alto de acceso. Sin embargo, algunos países se encuentran relegados con acceso bajo",
+
+  "grafico2.title": "Downloads y acceso a internet",
+  "grafico2.description": "Basándonos en la cantidad de descargas de cada iniciativa, relativas a la población de cada país, podemos también comparar la llegada que tienen las iniciativas a la población entre países. Aquí se ve una división aún más grande entre iniciativas con alta incidencia y iniciativas con mucha menor penetración.",
+
+  "grafico3.title": "¿Cuánta seguridad ofrecen las iniciativas para los datos que recogen?",
+  "grafico3.description": "Si agrupamos las iniciativas por los diferentes ejes que consideramos en cuanto a seguridad, se generan también, tres grupos principales:",
+  "grafico3.step0": 'Si agrupamos las iniciativas por sus características de seguridad podemos ver tres grandes grupos: ',
+  "grafico3.step1": 'Las que <b>no informan</b> sobre las medidas de seguridad, el <b>codigo es privativo</b>, la información se guarda en <b>servidores privados</b> y <b>fuera del país</b>, como: <span id="apps1"></span>',
+  "grafico3.step2": 'Las que <b>sí informan</b> sobre las medidas de seguridad, el <b>codigo es abierto</b>, la información se guarda en <b>servidores públicos</b> y <b>en el país</b>, como: <span id="apps2"></span>',
+  "grafico3.step3": 'Las que se encuentran en el medio, con una combinación de las variantes, como: <span id="apps3"></span>',
+
+  "grafico4.title": "¿Qué datos de usario recolectan?",
+  "grafico4.description": "Agrupando los datos que recolectan en 3 grandes grupos, podemos analizar como se posicionan las diferentes iniciativas respecto a la recolección de los mismos:",
+  "grafico4.step0": "En cuanto a <b>datos personales</b> (edad, documento, género), las aplicaciones se ubican en el siguiente espectro en cuanto a la cantidad de datos que recolectan",
+  "grafico4.step1": "En cuanto a <b>datos de ubicación</b> (dirección, ubicación), las aplicaciones se ubican en el siguiente espectro en cuanto a la cantidad de datos que recolectan",
+  "grafico4.step2": "En cuanto a <b>datos sensibles</b> (fotografía, datos de salud), las aplicaciones se ubican en el siguiente espectro en cuanto a la cantidad de datos que recolectan",
+
+  "grafico5.title": "AAA La importancia del consentimiento en los datos recogidos por las iniciativas",
+  "grafico5.description": "Qué sería mejor o no del punto de vista de la protección de los datos",
+  "grafico5.step0": 'A coleta/processamento de dados é precedida de um ato claro  afirmativo que estabeleça uma indicação livre, específica, informada e sem ambiguidades de concordância do titular dos dados quanto à coleta e/ou processamento de seus dados pessoais?',
+  "grafico5.step1": 'Como o consentimento é obtido?',
+  "grafico5.step2": 'A iniciativa permite que os usuários tenham controle efetivo sobre os usos?',
+  "grafico5.step3": 'A parte interessada pode retirar seu consentimento à iniciativa a qualquer momento?'
+
+
+}
